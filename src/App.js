e1d736fe72134b430997b06d217662d72abcd682
iganbold <itgel.ganbold1@gmail.com>
2020-10-15 03:09:06 -0700

    Add theming , Navbar, and header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Grommet, Box, Heading, Paragraph } from "grommet";
+import { grommet, dark } from "grommet/themes";
+import Nav from "./components/Nav";
+
+const THEMES = {
+  grommet,
+  dark,
+};
 
 function App() {
+  const [themeName, setThemeName] = React.useState("grommet");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>CryptoDance.XYZ</p>
-      </header>
-    </div>
+    <Grommet theme={THEMES[themeName || "grommet"]}>
+      <Box>
+        <Box pad="large">
+          <Nav />
+        </Box>
+        <Box align="center">
+          <Heading level="1" size="large" textAlign="center" margin="none">
+            Combined Order Book
+          </Heading>
+          <Paragraph size="xxlarge" textAlign="center">
+            A combined order book that takes the full order books from Bittrex
+            and Poloniex
+          </Paragraph>
+        </Box>
+      </Box>
+    </Grommet>
   );
 }
 
