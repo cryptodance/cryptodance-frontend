d17aa10512c5c9385cce85cba76ead21f855ee53
iganbold <itgel.ganbold1@gmail.com>
2020-10-15 02:41:39 -0700

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         CryptoDance.XYZ
-        </p>
+        <p>CryptoDance.XYZ</p>
       </header>
     </div>
   );
