5cff2fcc65e684a06b89e86211ef4363daafad2e
iganbold <itgel.ganbold1@gmail.com>
2020-10-15 03:07:47 -0700

    Create Navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Box, Text, ResponsiveContext, RoutedAnchor, Button } from "grommet";
+import { Money, Favorite } from "grommet-icons";
+
+const Nav = (props) => {
+  const size = React.useContext(ResponsiveContext);
+
+  return (
+    <Box
+      tag="header"
+      direction="row"
+      align="center"
+      justify="between"
+      width="xlarge"
+      alignSelf="center"
+      gap="medium"
+      pad={{ left: "medium", right: "small", vertical: "small" }}
+      {...props}
+    >
+      <RoutedAnchor
+        path="/"
+        icon={<Money size="large" />}
+        label={size !== "small" && <Text size="xlarge">CryptoDance</Text>}
+      />
+      <Box direction="row" gap="small">
+        <Button plain href="" target="_blank" rel="noreferrer">
+          {({ hover }) => (
+            <Box
+              pad={{ vertical: "small", horizontal: "medium" }}
+              round="xlarge"
+              background={hover ? "active" : "control"}
+              direction="row"
+              gap="small"
+            >
+              <Text icon> More</Text>
+            </Box>
+          )}
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Nav;
