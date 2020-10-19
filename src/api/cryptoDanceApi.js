07bdef4da95f78bde9f4953578eaf30c2bbbc17b
iganbold <itgel.ganbold1@gmail.com>
2020-10-19 02:48:47 -0700

    Get market summary from backend

--- src/api/cryptoDanceApi.js
+++ src/api/cryptoDanceApi.js
@@ -7,26 +7,11 @@ async function getOrderBooks(currencyPair) {
 }
 
 async function getExchangesSummary(currencyPair) {
-  return [
-    {
-      exchange: "bittrex",
-      exchangeUrl: "https://bittrex.com",
-      symbol: "BTC-ETH",
-      high: "0.03304434",
-      low: "0.03221759",
-      volume: "183.34866548",
-      percentChange: "1.93",
-    },
-    {
-      exchange: "poloniex",
-      exchangeUrl: "https://poloniex.com",
-      symbol: "BTC-ETH",
-      high: "0.03304499",
-      low: "0.03221475",
-      volume: "168.38156745",
-      percentChange: "-1.187576",
-    },
-  ];
+  let response = await fetch(
+    "https://cloud.cryptodance.xyz/marketsummary/btc_eth"
+  );
+  let marketsSummaries = response.json();
+  return marketsSummaries;
 }
 
 export { getOrderBooks, getExchangesSummary };
