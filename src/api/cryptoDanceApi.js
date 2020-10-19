ed7bf4c81cf29e6ca45276640bad790324ae49e7
iganbold <itgel.ganbold1@gmail.com>
2020-10-18 22:31:07 -0700

    Change backend url

--- src/api/cryptoDanceApi.js
+++ src/api/cryptoDanceApi.js
@@ -1,7 +1,5 @@
 async function getOrderBooks(currencyPair) {
-  let response = await fetch(
-    "http://cloud.cryptodance.xyz/api/v1/orderbooks/btc_eth"
-  );
+  let response = await fetch("http://cloud.cryptodance.xyz/orderbooks/btc_eth");
   let orderbook = response.json();
   return orderbook;
 }
